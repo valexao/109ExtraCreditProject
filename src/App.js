1be41815e0fa0171a43294da712984a9bcff75ff
Valexa Orelien <vorelien@stanford.edu>
2020-11-02 14:09:23 -0500

    Added links to the main javascript page and correct import statements, added header css to app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+import headerlogo from './headerlogo.png';
+import { Link } from 'react-router-dom';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  headimages: {
+    width: "700px",
+    height: "250px",
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={headerlogo} className={classes.headimages} alt="Logo: The Book Matcher" />
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+          <Link to="/HowItWorks">
+          How it Works
+          </Link></Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+          <Link to='/MyBookMatches'> My Books
+          </Link></Paper>
+        </Grid>
+      </Grid>
       </header>
+
+      <body className="App-body">
+      <Grid justify="center" container spacing={3}>
+        <Grid item xs={9}>
+      <Paper className={classes.paper}>
+      <Link to='/Matching'> Start Matching!
+          </Link>
+          </Paper>
+        </Grid>
+      </Grid>
+      </body>
     </div>
   );
 }
